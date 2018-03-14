afa7b7c50c30e4b44be848b71ca0ef3af6e5ce84
Lucas Montano <lucasmontano@gmail.com>
2018-03-14 11:26:38 -0300

    adding map with bottomsheet behavior for a better ux

--- app/src/main/java/com/lucasmontano/cities/view/activities/MainActivity.java
+++ app/src/main/java/com/lucasmontano/cities/view/activities/MainActivity.java
@@ -1,21 +1,34 @@
 package com.lucasmontano.cities.view.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.lucasmontano.cities.R;
 import com.lucasmontano.cities.data.City;
 import com.lucasmontano.cities.view.fragments.ListCityFragment;
-import com.lucasmontano.cities.view.fragments.MapDialog;
 import com.lucasmontano.cities.view.interfaces.ListCitiesListener;
 
-public class MainActivity extends AppCompatActivity implements ListCitiesListener {
+public class MainActivity extends AppCompatActivity implements ListCitiesListener,
+    OnMapReadyCallback {
 
   private ListCityFragment listCityFragment;
   private EditText mSearch;
+
+  private SupportMapFragment mapFragment;
+  private GoogleMap googleMap;
+  private BottomSheetBehavior<View> bottomSheetBehavior;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,27 @@ public class MainActivity extends AppCompatActivity implements ListCitiesListene
 
     initSearchView();
     initListCityFragment();
+    initMap();
+  }
+
+  /**
+   * Init and Place Map Fragment with BottomSheet Behavior.
+   */
+  private void initMap() {
+
+    View mBottomSheetMap = findViewById(R.id.bottom_sheet);
+    bottomSheetBehavior = BottomSheetBehavior.from(mBottomSheetMap);
+    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+    bottomSheetBehavior.setHideable(true);
+
+    FragmentManager fm = getSupportFragmentManager();
+    mapFragment = (SupportMapFragment) fm.findFragmentById(R.id.frame_map);
+    if (mapFragment == null) {
+      mapFragment = SupportMapFragment.newInstance();
+      fm.beginTransaction().replace(R.id.frame_map, mapFragment).commit();
+    }
+
+    mapFragment.getMapAsync(this);
   }
 
   /**
@@ -70,14 +104,23 @@ public class MainActivity extends AppCompatActivity implements ListCitiesListene
    */
   @Override
   public void onCityClick(City city) {
-    Bundle args = new Bundle();
-    args.putString(MapDialog.ARG_NAME, city.name + ", " + city.country);
-    args.putDouble(MapDialog.ARG_LAT, city.coord.lat);
-    args.putDouble(MapDialog.ARG_LON, city.coord.lon);
 
-    MapDialog mapDialog = new MapDialog();
-    mapDialog.setArguments(args);
-    mapDialog.show(getSupportFragmentManager(), "DIALOG_MAP");
+    if (googleMap == null) return;
+
+    LatLng latlng = new LatLng(city.coord.lat, city.coord.lon);
+    googleMap.addMarker(new MarkerOptions().position(latlng).title(city.name));
+    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 8f));
+    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+  }
+
+  /**
+   * On Map Loaded.
+   *
+   * @param googleMap google map android v2
+   */
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+    this.googleMap = googleMap;
   }
 
   @Override
